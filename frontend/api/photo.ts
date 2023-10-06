07df42f3a9580a50cc6068ec2748a6ccb067cc2d
Jacob Schillemans <jacob.schillemans@gmail.com>
2023-10-07 00:08:39 +0200

    Photo.ts

--- frontend/api/photo.ts
+++ frontend/api/photo.ts
@@ -1,56 +1,43 @@
 import axios from 'axios';
-import fs from 'fs';
 
-async function photoGenerator(location: string): Promise<void> {
-  // Define your API key
-  const apiKey = 'AIzaSyAWEPohy9CdHpz6j8-_zLDRsSWoDI9b2YU';
-  // Step 1: Perform a Place Search
-  const searchEndpoint = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
-  const searchParams = {
-    query: location,
-    key: apiKey,
-  };
+async function imageGenerator(location: string): Promise<string | null> {
+    // Your Unsplash API key
+    const apiKey = '8lUqDkIz_wzBmP1humc8zMF2U76DM_P8tiJsGcAmWOE';
+    // Text input for the image search
+    const query = location;
+    // API endpoint for searching photos
+    const apiUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${apiKey}`;
 
-  try {
-    const response = await axios.get(searchEndpoint, { params: searchParams });
-
-    if (response.status === 200) {
-      const data = response.data;
-      // Check if there are results
-      if (data.results && data.results.length > 0) {
-        // Extract the first result's photo_reference (you can loop through results if needed)
-        const photoReference = data.results[0].photos?.[0]?.photo_reference;
-
-        if (photoReference) {
-          // Step 2: Use the photo_reference to fetch the image
-          const photoEndpoint = 'https://maps.googleapis.com/maps/api/place/photo';
-          const photoParams = {
-            maxwidth: '400',
-            photoreference: photoReference,
-            key: apiKey,
-          };
-
-          const photoResponse = await axios.get(photoEndpoint, { params: photoParams });
-
-          if (photoResponse.status === 200) {
-            // Step 3: Save the image to a file
-            fs.writeFileSync('photo.jpg', photoResponse.data);
-            console.log('Photo downloaded successfully.');
-          } else {
-            console.error(`Error downloading photo: ${photoResponse.status} - ${photoResponse.statusText}`);
-          }
+    try {
+        // Send a GET request to the Unsplash API
+        const response = await axios.get(apiUrl);
+        // Get the URL of the first image from the search results
+        const data = response.data;
+        if (data.results && data.results.length > 0) {
+            const imageUrl: string = data.results[0].urls.raw;
+            return imageUrl;
         } else {
-          console.error('No photo_reference found in the search results.');
+            console.error('No results found for the given location.');
+            return null;
         }
-      } else {
-        console.error('No results found for the search query.');
-      }
-    } else {
-      console.error(`Error searching for places: ${response.status} - ${response.statusText}`);
+    } catch (error) {
+        console.error(`Error: ${(error as Error).message}`);
+        return null;
     }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
 }
 
-photoGenerator('Brussels');
+async function getImage(city: string) {
+    const imageUrl = await imageGenerator(city);
+    if (imageUrl) {
+        console.log('Image URL:', imageUrl);
+        // You can use imageUrl directly in your application or return it as needed
+    } else {
+        console.log('Image not found.');
+    }
+}
+
+// Example usage
+const city = 'Brussels';
+getImage(city);
+
+
