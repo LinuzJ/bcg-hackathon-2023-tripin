898c257fb2e3f78ca65255fabf899d0c9f6ef745
LinuzJ <linusjern00@gmail.com>
2023-10-06 15:12:19 +0200

    add cost calc

---
+++ backend/trips/cost_calculatot.py
@@ -0,0 +1,10 @@
+def car_cost(distance: float) -> float:
+    return 1.0
+
+
+def train_cost(distance: float) -> float:
+    return 1.0
+
+
+def plane_cost(distance: float) -> float:
+    return 1.0
