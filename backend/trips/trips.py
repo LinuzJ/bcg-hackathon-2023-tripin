5e3ffc932fe7e48c67d13a25790ee72f56c42f33
LinuzJ <linusjern00@gmail.com>
2023-10-06 21:18:32 +0200

    undo fix :D

--- backend/trips/trips.py
+++ backend/trips/trips.py
@@ -6,11 +6,10 @@ from openapi.openapi_client import fetch_trips
 
 def generate_trips(input):
     trips = fetch_trips(input)
-    input_dict = json.loads(input)
 
     # Calculate emissions based on trips
     for trip in trips["trips"]:
-        start = input_dict["starting_position"]
+        start = input["starting_position"]
         end = trip["position"]
         trans_type = trip["transportation"]
         emissions = calculate_emissions(start, end, trans_type)
