329e918a7ceba31e9b0d2aed027a583d3c066919
LinuzJ <linusjern00@gmail.com>
2023-10-06 20:14:21 +0200

    yolo cors

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 from trips.trips import generate_trips
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/generate-trips', methods=['POST'])
+@cross_origin()
 def trips():
     data = request.json
 
