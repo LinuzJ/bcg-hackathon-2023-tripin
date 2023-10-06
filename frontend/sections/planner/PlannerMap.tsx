ec95613bbc6c276ed3d0dc41e64b9f43414072eb
Mathias Raa <skraagen@hotmail.com>
2023-10-06 21:38:09 +0200

    frontend steps

---
+++ frontend/sections/planner/PlannerMap.tsx
@@ -0,0 +1,162 @@
+import GLMap, { Marker, MapProvider, useMap, LngLatLike } from "react-map-gl";
+import { HexagonLayer } from "@deck.gl/aggregation-layers/typed";
+import DeckGL from "@deck.gl/react/typed";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import {
+  lightingEffect,
+  material,
+  INITIAL_VIEW_STATE,
+  colorRange,
+} from "./mapConfig";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { MapPin, MapPinLine } from "@phosphor-icons/react";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+
+const dummyData = [
+  {
+    id: 0,
+    title: "Mongolia",
+    description: "Hei",
+    image:
+      "https://a.cdn-hotels.com/gdcs/production61/d1121/6dfd3cfe-d31b-4514-b3af-aee8536223d1.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+  },
+  {
+    id: 1,
+    title: "Düsseldorf",
+    description: "Hei",
+    image:
+      "https://a.cdn-hotels.com/gdcs/production61/d1121/6dfd3cfe-d31b-4514-b3af-aee8536223d1.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+  },
+];
+
+const API_KEY =
+  "pk.eyJ1IjoibWF0cmFhIiwiYSI6ImNreGMweHUwNjB0OGsycG83d3B3N2d4N2kifQ.sn_yCa6tkatkbAs_QSQxLQ";
+
+const PlannerMap: React.FC = () => {
+  const mapRef = useRef();
+
+  // useEffect(() => {
+  //   const map = new mapboxgl.Map({
+  //     container: mapContainerRef.current,
+  //     style: "mapbox://styles/lmaps/ckl6t1boq578819qod5v7ynby",
+  //     center: markerLngLat,
+  //     zoom: 13,
+  //   });
+
+  //   setMap(map);
+  // }, []);
+
+  const [center, setCenter] = useState<LngLatLike>([100, 50]);
+
+  return (
+    <>
+      <MapProvider>
+        <Map center={center} />
+      </MapProvider>
+      <Box
+        sx={{
+          width: "100vw",
+          bgcolor: "background.paper",
+          bottom: 0,
+          left: 0,
+          boxShadow: (theme) => theme.customShadows.z24,
+          p: 2,
+        }}
+      >
+        <Stack spacing={2}>
+          <Typography variant="h3">Travel Suggestions</Typography>
+          {dummyData.map((data) => (
+            <Card key={data.id}>
+              <CardActionArea
+                onClick={() => setCenter([100, 50])}
+                sx={{
+                  display: "flex",
+                  position: "relative",
+                  alignItems: "stretch",
+                  // border: "2px solid",
+                  borderColor: "primary.main",
+                }}
+              >
+                <Box
+                  sx={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+                >
+                  <CardHeader title={data.title} sx={{ py: 1, px: 2 }} />
+                  <CardContent sx={{ py: 1, px: 2 }}>
+                    <Typography variant="body1">{data.description}</Typography>
+                  </CardContent>
+                </Box>
+                <CardMedia>
+                  <Box
+                    component="img"
+                    sx={{ width: 150, height: 1 }}
+                    src={data.image}
+                  />
+                </CardMedia>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+const Map: React.FC<{ center: LngLatLike }> = ({ center }) => {
+  const { map } = useMap();
+
+  useEffect(() => {
+    map?.flyTo({ center: center });
+  }, [center, map]);
+
+  return (
+    <GLMap
+      // @ts-ignore
+      controller={true}
+      style={{ height: "50vh" }}
+      mapboxAccessToken={API_KEY}
+      id="map"
+      mapStyle="mapbox://styles/mapbox/light-v11"
+    >
+      {true && (
+        <Marker longitude={100} latitude={50}>
+          <Box
+            sx={{
+              width: 0,
+              height: 0,
+              "& span": {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                boxSizing: "border-box",
+                width: 30,
+                height: 30,
+                color: "error.main",
+                borderRadius: "50%",
+                cursor: "pointer",
+                transformOrigin: "0 0",
+                transform: "translate(-50%, -30px)",
+              },
+            }}
+          >
+            <span>
+              <MapPin weight="fill" size={30} />
+            </span>
+          </Box>
+        </Marker>
+      )}
+    </GLMap>
+  );
+};
+
+export default PlannerMap;
--- frontend/sections/planner/index.tsx
+++ frontend/sections/planner/index.tsx
@@ -30,6 +30,8 @@ import {
 } from "@phosphor-icons/react";
 import { useState } from "react";
 import axios from "axios";
+import PlannerForm from "./PlannerForm";
+import PlannerMap from "./PlannerMap";
 
 const climates = [
   { id: "tropical", name: "Tropical", icon: TreePalm },
@@ -58,7 +60,7 @@ function valueDuration(value: number) {
   return `${value} weeks`;
 }
 
-interface FormStateProps {
+export interface FormStateProps {
   starting_position: PlaceType;
   activity: string[];
   climate: string;
@@ -78,12 +80,12 @@ const HeaderTravelInput: React.FC = () => {
     },
     activity: ["relaxing"],
     climate: "tropical",
-    budget: [0, 3000],
+    budget: [0, 300],
     time_of_year: "winter",
     duration: 2,
   });
 
-  const [travelInputOpen, setTravelInputOpen] = useState(true);
+  const [formStep, setFormStep] = useState(0);
 
   const onFormStateChange = (
     key: keyof FormStateProps,
@@ -115,230 +117,21 @@ const HeaderTravelInput: React.FC = () => {
     setFormState(updatedFormState);
   };
 
-  const handleSubmission = async () => {
-    setTravelInputOpen(false);
-
-    const data = {
-      starting_position:
-        formState.starting_position.structured_formatting.main_text,
-      activity: formState.activity,
-      climate: formState.climate,
-      budget: formState.budget,
-      time_of_year: formState.time_of_year,
-      duration: formState.duration,
-    };
-
-    const API_URL = "http://127.0.0.1:8000/generate-trips";
-    const headers = {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    };
-
-    const response = await axios
-      .post(API_URL, data, { headers })
-      .catch((error) => {
-        console.log("Error in get multiple suggestions:" + error);
-        return null;
-      });
-
-    console.log(response);
+  const onFormSuccess = () => {
+    setFormStep(1);
   };
 
   return (
-    <Box p={2}>
-      <Accordion
-        expanded={travelInputOpen}
-        onChange={(e) => setTravelInputOpen(!travelInputOpen)}
-      >
-        <AccordionSummary expandIcon={<CaretDown size={20} />}>
-          <Typography variant="h3">Find your trip</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Box sx={{ width: 1 }}>
-            <Stack direction="column" spacing={4}>
-              {/* Location */}
-              <Typography variant="h5">
-                {"Where are you trip'in from?"}
-              </Typography>
-              <FormControl>
-                <GoogleMaps
-                  value={formState?.starting_position}
-                  setValue={(v) =>
-                    v && onFormStateChange("starting_position", v)
-                  }
-                />
-              </FormControl>
-              {/* Period */}
-              <Typography variant="h5">{"When are you going?"}</Typography>
-              <ToggleButtonGroup
-                value={formState?.time_of_year}
-                onChange={(_, v) => v && onFormStateChange("time_of_year", v)}
-                exclusive
-                fullWidth
-              >
-                <ToggleButton value="winter">Winter</ToggleButton>
-                <ToggleButton value="spring">Spring</ToggleButton>
-                <ToggleButton value="summer">Summer</ToggleButton>
-                <ToggleButton value="autumn">Autumn</ToggleButton>
-              </ToggleButtonGroup>
-              {/* Location */}
-              <Typography variant="h5">
-                {"What's the duration of your travels?"}
-              </Typography>
-              <div>
-                <Stack
-                  spacing={2}
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="center"
-                  px={4}
-                  pt={2}
-                >
-                  <Slider
-                    valueLabelDisplay="on"
-                    color="primary"
-                    valueLabelFormat={valueDuration}
-                    sx={{
-                      height: 20,
-                      "& .MuiSlider-thumb": {
-                        height: 25,
-                        width: 25,
-                        border: "2px solid",
-                        borderColor: "primary.main",
-                        bgcolor: "grey.100",
-                        boxShadow: (theme) => theme.customShadows.z12,
-                      },
-                    }}
-                    value={formState?.duration}
-                    max={8}
-                    min={1}
-                    step={1}
-                    onChange={(_, v) =>
-                      onFormStateChange("duration", v as number)
-                    }
-                  />
-                </Stack>
-              </div>
-
-              {/* Temperature */}
-              <Typography variant="h5">
-                {"What is your preferred climate?"}
-              </Typography>
-              <div>
-                <Grid container spacing={1} sx={{ width: 1 }}>
-                  {climates.map((c) => (
-                    <Grid key={c.id} item xs={6}>
-                      <Button
-                        variant="contained"
-                        onClick={() => onFormStateChange("climate", c.id)}
-                        sx={{
-                          width: 1,
-                          boxShadow: (theme) => theme.customShadows.z16,
-                          bgcolor:
-                            formState?.climate == c.id
-                              ? "primary.main"
-                              : "common.white",
-                          color:
-                            formState?.climate == c.id
-                              ? "common.white"
-                              : "text.secondary",
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                        }}
-                      >
-                        <c.icon size={30} />
-                        {c.name}
-                      </Button>
-                    </Grid>
-                  ))}
-                </Grid>
-              </div>
-              {/* Activity */}
-              <Typography variant="h5">
-                {"What is your preferred activity?"}
-              </Typography>
-              <div>
-                <Grid container spacing={1} sx={{ width: 1 }}>
-                  {activityLevels.map((c) => (
-                    <Grid key={c.id} item xs={4}>
-                      <Button
-                        variant="contained"
-                        onClick={() => onFormStateChange("activity", c.id)}
-                        sx={{
-                          width: 1,
-                          boxShadow: (theme) => theme.customShadows.z16,
-                          bgcolor: formState?.activity.includes(c.id)
-                            ? "primary.main"
-                            : "common.white",
-                          color: formState?.activity.includes(c.id)
-                            ? "common.white"
-                            : "text.secondary",
-                          display: "flex",
-                          flexDirection: "column",
-                          alignItems: "center",
-                          "&:hover": {
-                            bgcolor: formState?.activity.includes(c.id)
-                              ? "primary.main"
-                              : "grey.50",
-                          },
-                        }}
-                      >
-                        <c.icon size={30} />
-                        {c.name}
-                      </Button>
-                    </Grid>
-                  ))}
-                </Grid>
-              </div>
-              {/* Budget */}
-              <Typography variant="h5">{"What is your budget?"}</Typography>
-              <div>
-                <Stack
-                  spacing={2}
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="center"
-                  px={4}
-                  pt={2}
-                >
-                  <Slider
-                    valueLabelDisplay="on"
-                    valueLabelFormat={valuetext}
-                    color="primary"
-                    sx={{
-                      height: 20,
-                      "& .MuiSlider-thumb": {
-                        height: 25,
-                        width: 25,
-                        border: "2px solid",
-                        borderColor: "primary.main",
-                        bgcolor: "grey.100",
-                        boxShadow: (theme) => theme.customShadows.z12,
-                      },
-                    }}
-                    getAriaLabel={() => "Budget range"}
-                    value={formState?.budget}
-                    max={3000}
-                    step={100}
-                    onChange={(_, v) =>
-                      onFormStateChange("budget", v as [number, number])
-                    }
-                  />
-                </Stack>
-              </div>
-              <Button
-                onClick={handleSubmission}
-                size="large"
-                variant="contained"
-              >
-                Find my Trip
-              </Button>
-            </Stack>
-          </Box>
-        </AccordionDetails>
-      </Accordion>
-      {!travelInputOpen && (
+    <>
+      <Box p={2}>
+        {formStep == 0 && (
+          <PlannerForm
+            formState={formState}
+            onFormStateChange={onFormStateChange}
+            onSuccess={onFormSuccess}
+          />
+        )}
+        {/* {!travelInputOpen && (
         <Box
           sx={{
             width: 1,
@@ -347,11 +140,13 @@ const HeaderTravelInput: React.FC = () => {
             alignItems: "center",
             height: "50%",
           }}
-        >
+          >
           <CircularProgress />
-        </Box>
-      )}
-    </Box>
+          </Box>
+        )} */}
+      </Box>
+      {formStep == 1 && <PlannerMap />}
+    </>
   );
 };
 
